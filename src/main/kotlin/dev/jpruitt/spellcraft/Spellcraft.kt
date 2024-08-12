a70776e7ff51ef66d66110ff07f87a1a08b9938d
James Pruitt <jamescpruitt@gmail.com>
2024-08-12 04:04:17 +0000

    Working example of use()

--- src/main/kotlin/dev/jpruitt/spellcraft/Spellcraft.kt
+++ src/main/kotlin/dev/jpruitt/spellcraft/Spellcraft.kt
@@ -5,20 +5,32 @@ import org.slf4j.LoggerFactory
 import net.minecraft.item.Item
 import net.minecraft.world.World
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.entity.LightningEntity
+import net.minecraft.entity.EntityType
 import net.minecraft.registry.Registry
 import net.minecraft.registry.Registries
 import net.minecraft.util.Identifier
 import net.minecraft.item.ItemStack
 import net.minecraft.util.TypedActionResult
 import net.minecraft.util.Hand
+import net.minecraft.util.ActionResult
 import eu.pb4.polymer.core.api.item.SimplePolymerItem
 import eu.pb4.polymer.core.api.item.PolymerItem
 import net.minecraft.item.Items
+import net.minecraft.item.ItemUsageContext
 import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.text.Text
+import eu.pb4.polymer.core.api.item.PolymerItemUtils
 
-class TestItem(settings: Settings) : Item(settings), PolymerItem {
+class Scroll(settings: Settings) : Item(settings), PolymerItem {
+
+    private val logger = LoggerFactory.getLogger("spellcraft")
+
 	override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
-		//logger.info("Item used")
+		val target = user.getBlockPos().offset(user.getHorizontalFacing(), 10).toCenterPos()
+		var bolt = LightningEntity(EntityType.LIGHTNING_BOLT, world)
+		bolt.setPosition(target)
+		world.spawnEntity(bolt)
 		return TypedActionResult.success(user.getStackInHand(hand))
 	}
 	
@@ -30,15 +42,13 @@ class TestItem(settings: Settings) : Item(settings), PolymerItem {
 object Spellcraft : ModInitializer {
     private val logger = LoggerFactory.getLogger("spellcraft")
 	private val item_settings = Item.Settings()
-	private val item = TestItem(item_settings)
+	private val scroll = Scroll(item_settings)
 
 	override fun onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		logger.info("Hello Fabric world!")
-
-		Registry.register(Registries.ITEM, Identifier.of("tutorial", "custom_item"), item)
-		Registry.register(Registries.ITEM, Identifier.of("tutorial", "custom_item"), SimplePolymerItem(item_settings, item))
+		Registry.register(Registries.ITEM, Identifier.of("spellcraft", "scroll"), scroll)
 	}
 }
